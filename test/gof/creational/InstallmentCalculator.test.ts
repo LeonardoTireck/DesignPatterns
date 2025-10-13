b9230516e6ce8095418b0b24e7694b59d05d5abf
Leonardo Tireck <leotireck@gmail.com>
2025-10-13 10:43:54 -0300

    add price installment calculator

--- test/gof/creational/InstallmentCalculator.test.ts
+++ test/gof/creational/InstallmentCalculator.test.ts
@@ -1,4 +1,7 @@
-import { SACInstallmentCalculator } from "../../../gof/creational/abstract_factory/InstallmentCalculator";
+import {
+  PriceInstallmentCalculator,
+  SACInstallmentCalculator,
+} from "../../../gof/creational/abstract_factory/InstallmentCalculator";
 import { MortgageLoan } from "../../../gof/creational/abstract_factory/Loan";
 
 test("Deve calcular as parcelas utilizando SAC", function () {
@@ -16,4 +19,21 @@ test("Deve calcular as parcelas utilizando SAC", function () {
   expect(installments.at(239)?.balance).toBe(0);
   expect(installments.at(239)?.amount).toBe(420.14);
   expect(installments.at(239)?.interest).toBe(3.47);
+});
+
+test("Deve calcular as parcelas utilizando PRICE", function () {
+  const installmentCalculator = new PriceInstallmentCalculator();
+  const loan = MortgageLoan.create(100000, 10000, 240);
+  const installments = installmentCalculator.calculate(loan);
+  expect(installments).toHaveLength(240);
+  expect(installments.at(0)?.number).toBe(1);
+  expect(installments.at(0)?.amortization).toBe(131.69);
+  expect(installments.at(0)?.amount).toBe(965.02);
+  expect(installments.at(0)?.interest).toBe(833.33);
+  expect(installments.at(0)?.balance).toBe(99868.31);
+  expect(installments.at(239)?.number).toBe(240);
+  expect(installments.at(239)?.amortization).toBe(957.03);
+  expect(installments.at(239)?.balance).toBe(0);
+  expect(installments.at(239)?.amount).toBe(965.02);
+  expect(installments.at(239)?.interest).toBe(7.99);
 });
