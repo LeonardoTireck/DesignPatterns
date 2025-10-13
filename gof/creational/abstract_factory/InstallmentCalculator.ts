c2ab050f78449323bf6adfc6d238057e2c1a5c3b
Leonardo Tireck <leotireck@gmail.com>
2025-10-13 09:57:36 -0300

    add installment calculator

--- gof/creational/abstract_factory/InstallmentCalculator.ts
+++ gof/creational/abstract_factory/InstallmentCalculator.ts
@@ -10,7 +10,27 @@ export class SACInstallmentCalculator implements InstallmentCalculator {
   calculate(loan: Loan): Installment[] {
     const installments: Installment[] = [];
     let balance = currency(loan.amount);
-
+    let rate = loan.rate / 100 / 12;
+    let installmentNumber = 1;
+    let amortization = currency(balance.value / loan.installments);
+    while (balance.value > 0.1) {
+      let interest = currency(balance.value * rate);
+      let updatedBalance = currency(balance.value + interest.value);
+      let amount = currency(interest.value + amortization.value);
+      balance = currency(updatedBalance.value - amount.value);
+      if (balance.value < 0.1) balance = currency(0);
+      installments.push(
+        new Installment(
+          loan.loanId,
+          installmentNumber,
+          amount.value,
+          amortization.value,
+          interest.value,
+          balance.value,
+        ),
+      );
+      installmentNumber++;
+    }
     return installments;
   }
 }
--- test/gof/creational/InstallmentCalculator.test.ts
+++ test/gof/creational/InstallmentCalculator.test.ts
@@ -6,4 +6,14 @@ test("Deve calcular as parcelas utilizando SAC", function () {
   const loan = MortgageLoan.create(100000, 10000, 240);
   const installments = installmentCalculator.calculate(loan);
   expect(installments).toHaveLength(240);
+  expect(installments.at(0)?.number).toBe(1);
+  expect(installments.at(0)?.amortization).toBe(416.67);
+  expect(installments.at(0)?.amount).toBe(1250);
+  expect(installments.at(0)?.interest).toBe(833.33);
+  expect(installments.at(0)?.balance).toBe(99583.33);
+  expect(installments.at(239)?.number).toBe(240);
+  expect(installments.at(239)?.amortization).toBe(416.67);
+  expect(installments.at(239)?.balance).toBe(0);
+  expect(installments.at(239)?.amount).toBe(420.14);
+  expect(installments.at(239)?.interest).toBe(3.47);
 });
